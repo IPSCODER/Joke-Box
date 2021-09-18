efbf3916fe8ce0525746514d9915295a96ee1646
parrya <psawant0015@gmail.com>
2021-09-18 08:41:47 +0530

    joke box

---
+++ style.js
@@ -0,0 +1,31 @@
+{/* <div id="joke" class="joke">Awsome joke is loading....</div> */}
+{/* <button id="jokeBtn" class="btn">Next Joke</button> */}
+// fetch('https://restcountries.eu/rest/v2/name/nepal');
+
+// Promise 
+
+// GET https://icanhazdadjoke.com/
+
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+const generateJokes = () => {
+    const setHeader = {
+        headers : {
+            Accept : "application/json"
+        }
+    }
+
+    fetch("https://icanhazdadjoke.com/", setHeader)
+    .then((res) =>
+      res.json()
+     )
+    .then((data) =>{
+        jokes.innerHTML = data.joke;
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
+
+jokeBtn.addEventListener("click",generateJokes);
+generateJokes();
